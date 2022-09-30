b2656146487a132c3eee6a691b60e913ee8f1ef3
Mark Phillipson <MPhillipson0@Gmail.com>
2022-09-30 12:46:51 +0100

    setup locally and change the theme to default bootstrap and published to VM

--- ArmMordanizerGUI/Models/Mapping.cs
+++ ArmMordanizerGUI/Models/Mapping.cs
@@ -7,7 +7,7 @@ namespace ArmMordanizerGUI.Models
         [Key]
         public int Id { get; set; }
         [Required]
-        public string Name { get; set; }
+        public string Name { get; set; } = null!;
         public int DisplayOrder { get; set; }
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
     }
