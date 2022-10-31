babb0d8eb4a63d1d235b5f9d8cca52dabd2d38f8
Mark Phillipson <MPhillipson0@Gmail.com>
2022-10-31 09:28:18 +0000

    more ignore

--- ArmMordanizerGUI/obj/Release/net6.0/PubTmp/Out/wwwroot/js/site.js
+++
@@ -1,9 +0,0 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
-function RemoveClass(elementId, className) {
-    document.getElementById(elementId).className =
-        document.getElementById(elementId).className
-            .replace(new RegExp('(?:^|\\s)' + className + '(?:\\s|$)'), ' ');
-}
